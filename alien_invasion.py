360eea2ee9e258583bbaf97c02f18d3fdcfdf539
Rares Popa <rarpopa@hotmail.com>
2020-06-21 20:01:38 +0800

    Added ship controls

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,14 +12,15 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
 
     #Make a ship
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
 
     #Set background colour
     bg_colour = (230,230,230)
 
     #Start the main loop for the game
     while True:
-        gf.check_events()
+        gf.check_events(ship)
+        ship.update()
         gf.update_screen(ai_settings, screen, ship)
 
 run_game()
