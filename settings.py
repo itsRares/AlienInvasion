90ace858ee494b280147c2b6285cd9a13d8c9276
Rares Popa <rarpopa@hotmail.com>
2020-06-21 17:46:36 +0800

    Added ship to window

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings():
+    """A class to store all settings for Alien Invasion"""
+
+    def __init__(self):
+        """Initialize the game's settings"""
+        #Screen Settings
+        self.screen_width = 1000
+        self.screen_height = 650
+        self.bg_colour = (230,230,230)
