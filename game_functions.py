f63181e0550df6aaa96a4f31974e35c0de8ffd7d
Rares Popa <rarpopa@hotmail.com>
2020-06-21 21:07:19 +0800

    More consise and readable code

--- game_functions.py
+++ game_functions.py
@@ -9,16 +9,10 @@ def check_events(ship):
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
+            check_keydown_events(event, ship)
 
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
+            check_keyup_events(event, ship)
 
 def update_screen(ai_settings, screen, ship):
     """Updates images on the screen and flip to new screen"""
@@ -28,3 +22,17 @@ def update_screen(ai_settings, screen, ship):
 
     #Make the most recently drawn screen visible
     pygame.display.flip()
+
+def check_keydown_events(event, ship):
+    """Respond to keypresses"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+def check_keyup_events(event, ship):
+    """Respond to key releases."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
