ced0ccb04ce5ea47d944b087b85d3a6c9f25518e
Rares Popa <rarpopa@hotmail.com>
2020-06-22 11:15:43 +0800

    Added alien ship

--- game_functions.py
+++ game_functions.py
@@ -16,7 +16,7 @@ def check_events(ai_settings, screen, ship, bullets):
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
-def update_screen(ai_settings, screen, ship, bullets):
+def update_screen(ai_settings, screen, ship, alien, bullets):
     """Updates images on the screen and flip to new screen"""
     #Redraw the screen during each pass through the loop
     screen.fill(ai_settings.bg_colour)
@@ -26,6 +26,7 @@ def update_screen(ai_settings, screen, ship, bullets):
         bullet.draw_bullet()
 
     ship.blitme()
+    alien.blitme()
 
     #Make the most recently drawn screen visible
     pygame.display.flip()
