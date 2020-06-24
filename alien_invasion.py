b4e5a0136247e9ff1e63834010b0a725756f5724
Rares Popa <rarpopa@hotmail.com>
2020-06-24 16:07:53 +0800

    Added scoreboard feature

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ from pygame.sprite import Group
 
 from settings import Settings
 from game_stats import GameStats
+from scoreboard import Scoreboard
 from button import Button
 from ship import Ship
 from alien import Alien
@@ -15,11 +16,12 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    #Create an instance to store game statistics
+    #Create an instance to store game statistics and create scoreboard
     stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
 
     #Make the play button
-    play_button = Button(ai_settings, screen, "Play")
+    play_button = Button(ai_settings, screen, "PLAY")
 
     #Make a ship
     ship = Ship(ai_settings, screen)
@@ -36,9 +38,9 @@ def run_game():
         
         if stats.game_active:
             ship.update()
-            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets)
             gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
         
-        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)
 
 run_game()
