5a95aaca64c1d7feee769dc55b54c823a3efed2d
Rares Popa <rarpopa@hotmail.com>
2020-06-24 10:41:05 +0800

    Adding stats

---
+++ game_stats.py
@@ -0,0 +1,14 @@
+class GameStats():
+    """Track statistics for alien invasion"""
+
+    def __init__(self, ai_settings):
+        """Initialize settings"""
+        self.ai_settings = ai_settings
+        self.reset_stats()
+
+        #Start alien invasion in a active state
+        self.game_active = True
+
+    def reset_stats(self):
+        """Initialize statistics that can change during the game"""
+        self.ship_left = self.ai_settings.ship_limit
